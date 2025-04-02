f82cc740073859511e7d52061c19fa1585f986cf
jafar-hussein <jafarhussein6@gmail.com>
2025-04-02 22:21:03 +0200

    Fixade map

---
+++ lib/MapScreen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart'; // Ensure you're using latlong2
+
+class MapScreen extends StatefulWidget {
+  const MapScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  final MapController mapController = MapController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FlutterMap(
+        mapController: mapController,
+        options: MapOptions(
+          initialCenter: LatLng(59.3293, 18.0686), // Coordinates of Stockholm
+          initialZoom: 13,
+        ),
+        children: [
+          TileLayer(
+            urlTemplate:
+                "https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=",
+            subdomains: ['a', 'b', 'c'],
+          ),
+          MarkerLayer(
+            markers: [
+              Marker(
+                point: LatLng(59.3293, 18.0686),
+                width: 80,
+                height: 80,
+                child: Builder(
+                  builder:
+                      (ctx) => Container(
+                        child: Icon(
+                          Icons.location_on,
+                          size: 40.0,
+                          color: Colors.red,
+                        ),
+                      ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'MapScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,33 +30,26 @@ class _MyAppState extends State<MyApp> {
 
     // Use copyWith to modify only specific aspects of the theme.
     return base.copyWith(
-      primaryColor:
-          Colors
-              .blueGrey[800], // This sets the primary color used in various widgets
+      primaryColor: Colors.blueGrey[800],
       colorScheme: base.colorScheme.copyWith(
-        primary: Colors.blueGrey[800], // Primary color used by Material widgets
-        secondary:
-            Colors.tealAccent[700], // Secondary color used by Material widgets
-        surface: Colors.grey[850], // Surface color for cards, sheets, etc.
-        background:
-            Colors.grey[800], // Lighter background color for Scaffold, etc.
-        onSurface: Colors.white, // Text/icon color on top of surface colors
-        onPrimary: Colors.white, // Text/icon color on top of primary colors
-        onSecondary: Colors.black, // Text/icon color on top of secondary colors
+        primary: Colors.blueGrey[800],
+        secondary: Colors.tealAccent[700],
+        surface: Colors.grey[850],
+        background: Colors.grey[800],
+        onSurface: Colors.white,
+        onPrimary: Colors.white,
+        onSecondary: Colors.black,
       ),
       appBarTheme: AppBarTheme(
-        backgroundColor: Colors.grey[900], // Color of the AppBar
-        foregroundColor: Colors.white, // Color of items on the AppBar
+        backgroundColor: Colors.grey[900],
+        foregroundColor: Colors.white,
       ),
       buttonTheme: ButtonThemeData(
-        buttonColor: Colors.blueGrey[700], // Default button color
-        textTheme:
-            ButtonTextTheme.primary, // Use the primary color for button text
+        buttonColor: Colors.blueGrey[700],
+        textTheme: ButtonTextTheme.primary,
       ),
       textButtonTheme: TextButtonThemeData(
-        style: TextButton.styleFrom(
-          foregroundColor: Colors.tealAccent[700], // Color for text buttons
-        ),
+        style: TextButton.styleFrom(foregroundColor: Colors.tealAccent[700]),
       ),
     );
   }
@@ -64,11 +60,8 @@ class _MyAppState extends State<MyApp> {
       title: 'Parking App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light(), // Default light theme
-      darkTheme: customDarkTheme(), // Use the custom dark theme
-      themeMode:
-          _isDarkMode
-              ? ThemeMode.dark
-              : ThemeMode.light, // Toggle based on the state
+      darkTheme: customDarkTheme(), // Custom dark theme
+      themeMode: _isDarkMode ? ThemeMode.dark : ThemeMode.light,
       home: MyHomePage(
         title: 'Parking App',
         toggleTheme: toggleTheme,
@@ -95,133 +88,26 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool isLoggedIn = false;
-  final TextEditingController _usernameController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-
-  @override
-  void dispose() {
-    _usernameController.dispose();
-    _passwordController.dispose();
-    super.dispose();
-  }
-
-  void _showLoginDialog() {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Login'),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                TextField(
-                  controller: _usernameController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Username',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 8),
-                TextField(
-                  controller: _passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                if (_usernameController.text.isNotEmpty &&
-                    _passwordController.text.isNotEmpty) {
-                  Navigator.of(context).pop();
-                  setState(() {
-                    isLoggedIn = true;
-                  });
-                } else {
-                  print("Please enter both username and password.");
-                }
-              },
-              child: const Text('Login'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        title: Text(widget.title, style: TextStyle(color: Colors.white)),
-        centerTitle: true,
+      body: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: MapScreen(), // Embed the MapScreen directly
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[],
-        ),
-      ),
-      backgroundColor:
-          Theme.of(context).colorScheme.background, // Set the background color
       bottomNavigationBar: BottomAppBar(
         color: Theme.of(context).primaryColor,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              const Icon(FontAwesomeIcons.parking, color: Colors.white),
-              const Icon(FontAwesomeIcons.car, color: Colors.white),
-              const Icon(FontAwesomeIcons.mapMarkerAlt, color: Colors.white),
-              PopupMenuButton<String>(
-                icon: const Icon(FontAwesomeIcons.user, color: Colors.white),
-                offset:
-                    isLoggedIn ? const Offset(80, -160) : const Offset(50, -70),
-                onSelected: (String result) {
-                  if (result == 'Login') {
-                    _showLoginDialog();
-                  } else if (result == 'Logout') {
-                    setState(() {
-                      isLoggedIn = false;
-                    });
-                  } else if (result == 'isDarkMode') {
-                    widget.toggleTheme(!widget.isDarkMode);
-                  }
-                },
-                itemBuilder:
-                    (BuildContext context) => <PopupMenuEntry<String>>[
-                      if (!isLoggedIn)
-                        const PopupMenuItem<String>(
-                          value: 'Login',
-                          child: Text('Login'),
-                        ),
-                      if (isLoggedIn) ...[
-                        const PopupMenuItem<String>(
-                          value: 'Profile',
-                          child: Text('Profile'),
-                        ),
-                        const PopupMenuItem<String>(
-                          value: 'isDarkMode',
-                          child: Text('Toggle Dark Mode'),
-                        ),
-                        const PopupMenuItem<String>(
-                          value: 'Logout',
-                          child: Text('Logout'),
-                        ),
-                      ],
-                    ],
-              ),
-            ],
-          ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Icon(FontAwesomeIcons.parking, color: Colors.white),
+            Icon(FontAwesomeIcons.car, color: Colors.white),
+            Icon(FontAwesomeIcons.mapMarkerAlt, color: Colors.white),
+          ],
         ),
       ),
     );
